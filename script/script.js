e5336efc33ee266f42a912f1ee7c0e609d9adc7f
Stéphane LABROT <stephane.labrot@gmail.com>
2026-04-01 09:33:29 +0200

    Ajout de texte alternatif sur la photo de profil

--- script/script.js
+++ script/script.js
@@ -41,7 +41,7 @@ function createSkillsFromJSON() {
                     <div class="card skillsText">
                         <div class="card-body">
                             <img src="./images/${item.image}" alt="${item.title}" />
-                            <h4 class="card-title mt-3">${item.title}</h4>
+                            <h3 class="card-title mt-3 fs-4">${item.title}</h3>
                             <p class="card-text mt-3">${item.text}</p>
                         </div>
                     </div>
@@ -77,7 +77,7 @@ function createPortfolioFromJSON() {
                     <div class="card portfolioContent">
                     <img class="card-img-top" src="images/${item.image}" style="width:100%" alt="${item.title}">
                     <div class="card-body">
-                        <h4 class="card-title">${item.title}</h4>
+                        <h3 class="card-title fs-4">${item.title}</h3>
                         <p class="card-text">${item.text}</p>
                         <div class="text-center">
                             <a href="${item.link}" class="btn btn-success">Lien</a>
