9072160f451a2356fbca3b94c28168fab23089b6
Stéphane LABROT <stephane.labrot@gmail.com>
2026-04-09 17:25:25 +0200

    Ajustement des cartes projets

--- script/script.js
+++ script/script.js
@@ -74,16 +74,17 @@ function createPortfolioFromJSON() {
         const card = document.createElement("div")
         card.classList.add("col-lg-4", "mt-4")
         card.innerHTML = `
-                    <div class="card portfolioContent">
+                    <div class="card portfolioContent h-100">
                     <img class="card-img-top" src="images/${item.image}" style="width:100%" alt="${item.title}">
                     <div class="card-body">
                         <h3 class="card-title fs-4">${item.title}</h3>
                         <p class="card-text">${item.text}</p>
-                        <div class="text-center">
+                    </div>
+                    <div class="card-footer text-center">
                             <a href="${item.link}" class="btn btn-success" target="_blank">Lien</a>
                             ${item.websiteLink ? `<a href="${item.websiteLink}" class="btn btn-primary ms-2" target="_blank">Site Web</a>` : ""}
-                        </div>
                     </div>
+                    
                 </div>
                 `
 
